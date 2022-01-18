2cb1f66a408828a50d7bb72e092840c5625a9643
Adailton Junior <adailton_jr@hotmail.com>
2022-01-18 10:45:49 -0300

    add spring backend

---
+++ backend/src/main/java/com/calcme/CalcmeDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.calcme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcmeDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcmeDesafioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/calcme/CalcmeDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calcme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcmeDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
